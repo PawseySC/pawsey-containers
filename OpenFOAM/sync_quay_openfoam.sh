f8a4600df22e8af2fd7b369f115277df9c51040f
Marco De La Pierre <marco.delapierre@gmail.com>
2021-09-08 13:09:22 +0000

    openfoam/ sync_quay script

---
+++ OpenFOAM/sync_quay_openfoam.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+echo " ***** "
+echo " This is a script to sync DockerHub OpenFOAM images onto Quay.io."
+echo " Please ensure you are logged in to the container registry, otherwise push command will fail."
+echo " ***** "
+echo ""
+
+
+repo="pawsey/openfoam"
+# Using chunks, to avoid filling up the hard-drive of small machines
+of_tags_1="2.2.0 2.4.x 5.x 5.x_CFDEM"
+of_tags_2="7 8 v1712 v1812"
+of_tags_3="v1912 v2006 v2012"
+chunks="of_tags_1 of_tags_2 of_tags_3"
+
+
+# Using chunks, to avoid filling up the hard-drive of small machines
+for of_tags in $chunks ; do
+  for tag in ${!of_tags} ; do
+    image="$repo:$tag"
+    echo " .. Now syncing $image"
+    docker pull $image
+    docker tag $image quay.io/$image
+    docker push quay.io/$image
+    docker rmi quay.io/$image
+  done
+
+  for tag in ${!of_tags} ; do
+    image="$repo:$tag"
+    echo " .. Now syncing $image"
+    docker rmi $image
+  done
+done
+
+
+echo ""
+echo " Gone through all syncs. Done!"
+exit
+
