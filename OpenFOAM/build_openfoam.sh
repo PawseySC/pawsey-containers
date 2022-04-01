2095bba09f5f916743219202b8dcede588760698
Marco De La Pierre <marco.delapierre@gmail.com>
2022-04-01 01:48:22 +0000

    openfoam: final build_openfoam.sh, with better chunks and local image removal

--- OpenFOAM/build_openfoam.sh
+++ OpenFOAM/build_openfoam.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
 
+# Disk space requirements:
+# All images (no intermediate removal): about 110 GB
+# Images in chunks (intermediate removal): about 40 GB
+
+
 ### BEGIN OF EDITABLE: edit these variables to change which images are being built
 # Definition of base images, only to force pull them
 base="quay.io/pawsey/mpich-base"
@@ -9,12 +14,14 @@ base_mpich_vers="3.4.3"
 # Target repo
 repo="quay.io/pawsey/openfoam"
 # Using chunks, to avoid filling up the disk of small machines
-of_tags_1="2.2.0 2.4.x"
-of_tags_2="5.x 5.x_CFDEM"
-of_tags_3="7 8"
-of_tags_4="v1712 v1812"
-of_tags_5="v1912 v2006 v2012"
-chunks="of_tags_5 of_tags_4 of_tags_3 of_tags_2 of_tags_1"
+of_tags_1="2.2.0 2.4.x 5.x 5.x_CFDEM"
+of_tags_2="7 8"
+of_tags_3="v1712 v1812 v1912"
+of_tags_4="v2006 v2012"
+chunks="of_tags_1 of_tags_2 of_tags_3 of_tags_4"
+# 
+# Enable removal (in chunks) of local images, to save disk space
+enable_remove_local_image="0"
 ### END OF EDITABLE
 
 
@@ -39,9 +46,10 @@ cd $basedir
 # Force update starting images
 for os in $base_os_vers ; do
   for mpich in $base_mpich_vers ; do
-    base_image="${base}:${mpich}_ubuntu${os}"
+    base_tag="${mpich}_ubuntu${os}"
+    base_image="${base}:${base_tag}"
     echo " .. Force pulling base $base_image"
-    docker pull $base_image
+    docker pull $base_image &>out_base_pull_${base_tag}
   done
 done
 
@@ -60,18 +68,20 @@ for of_tags in $chunks ; do
     fi
     echo " .. In directory $(pwd)"
     echo " .. Now building $image"
-    docker build -t $image .
+    docker build -t $image . &>../../out_build_$tag
     echo " .. Now pushing $image"
-    docker push $image
+    docker push $image &>../../out_push_$tag
     cd ../..
   done
 
 # Remove local images to save disk space
-  for tag in ${!of_tags} ; do
-    image="$repo:$tag"
-    echo " .. Now removing local $image"
-    docker rmi $image
-  done
+  if [ "$enable_remove_local_image" != 0 ] ; then
+    for tag in ${!of_tags} ; do
+      image="$repo:$tag"
+      echo " .. Now removing local $image"
+      docker rmi $image &>out_rmi_$tag
+    done
+  fi
 done
 
 
