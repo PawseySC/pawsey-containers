92e7d050977bdaf478f8c7b4797d3319919768d2
Craig Meyer <cmeyer@pawsey.org.au>
2026-08-11 09:59:18 +0800

    Add basic GPU-MPI comms test

---
+++ setonix/mpi/tests/fixtures/gpu-mpi_comms.cpp
@@ -0,0 +1,444 @@
+/*!
+    \file test_gpu_mpi_comm.cpp
+    \brief Test GPU MPI communication using different MPI communicators and message sizes, and report timing statistics.
+    \details This test initializes the profiling utility, performs GPU-to-GPU communication 
+    using MPI while logging various metrics, and verifies the results. Note that this is a
+    deliberately trimmed down version of the original for simple functionality and rough performance testing
+*/
+
+#include <iostream>
+#include <vector>
+#include <chrono>
+#include <cmath>
+#include <algorithm>
+#include <random>
+#include <tuple>
+#include <thread>
+#include <getopt.h>
+#include <hip/hip_runtime.h>
+
+#ifdef USEOPENMP
+#include <omp.h>
+#endif
+#include <mpi.h>
+
+#define _GPU_TO_SECONDS 1.0/1000.0
+
+int ThisTask, NProcs;
+std::chrono::system_clock::time_point logtime;
+std::time_t log_time;
+char wherebuff[1000];
+std::string whenbuff;
+
+#define Where() sprintf(wherebuff,"[%04d] @%sL%d ", ThisTask,__func__, __LINE__);
+#define When() logtime = std::chrono::system_clock::now(); log_time = std::chrono::system_clock::to_time_t(logtime);whenbuff=std::ctime(&log_time);whenbuff.erase(std::find(whenbuff.begin(), whenbuff.end(), '\n'), whenbuff.end());
+#define LocalLogger() Where();std::cout<<wherebuff<<" : " 
+#define Rank0LocalLogger() Where();if (ThisTask==0) std::cout<<wherebuff<<" : " 
+#define LocalLoggerWithTime() Where();When(); std::cout<<wherebuff<<" ("<<whenbuff<<") : "
+#define Rank0LocalLoggerWithTime() Where();When(); if (ThisTask==0) std::cout<<wherebuff<<" ("<<whenbuff<<") : "
+#define LogMPITest() Rank0LocalLoggerWithTime()<<" running "<<mpifunc<< " test"<<std::endl;
+
+struct Options
+{
+    /// message size in GB
+    double msg_size = 1.0;
+    /// number of iterations
+    int Niter = 1;
+};
+
+/// usage
+void usage()
+{
+    Rank0LocalLogger()<<"Options: "<<std::endl;
+    Rank0LocalLogger()<<"  -s <max message size in GB> (default 1.0) "<<std::endl;
+    Rank0LocalLogger()<<"  -i <number of iterations for each test> (default 1) "<<std::endl;
+    MPI_Finalize();
+    exit(0);
+}
+
+///routine to get arguments from command line
+void GetArgs(int argc, char *argv[], Options &opt)
+{
+    int option;
+    while ((option = getopt(argc, argv, ":s:i:r:o:G:C:")) != EOF)
+    {
+        switch(option)
+        {
+            case 's':
+                opt.msg_size= atof(optarg);
+                break;
+            case 'i':
+                opt.Niter = atoi(optarg);
+                break;
+            case '?':
+                usage();
+                break;
+        }
+    }
+}
+
+
+/// \defgroup Device MPI (GPU-to-GPU) Performance 
+//@{
+
+void MPITestGPUCopy(Options &opt){
+
+    std::vector<double*> gpu_p1, gpu_p2;
+    std::vector<float> mpitransfertimes;
+    double nbytes = opt.msg_size * 1024.0 * 1024.0 * 1024.0;
+    double nelements = nbytes / sizeof(double);
+
+    int nDevices;
+    hipGetDeviceCount(&nDevices);
+    std::vector<double> senddata;
+    gpu_p1.resize(nDevices);
+    gpu_p2.resize(nDevices);
+    for (auto &x:gpu_p1) x=nullptr;
+    for (auto &x:gpu_p2) x=nullptr;
+
+
+    std::string mpifunc = "GPU_GPU_copy";
+    LogMPITest();
+    senddata.resize(nelements);
+    float timetaken;
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipMalloc((void**)&gpu_p1[idev], nbytes);
+        hipMalloc((void**)&gpu_p2[idev], nbytes);
+        // Copy source data to device once
+        hipMemcpy(gpu_p1[idev], senddata.data(), nbytes, hipMemcpyHostToDevice);
+
+        timetaken=0.0;
+        hipEvent_t gpuEventStart, gpuEventStop;
+        hipEventCreate(&gpuEventStart);
+        hipEventCreate(&gpuEventStop);
+        for (auto iter=0;iter<opt.Niter;iter++) {
+            hipEventRecord(gpuEventStart);
+            hipMemcpy(gpu_p2[idev], gpu_p1[idev], nbytes, hipMemcpyDeviceToDevice);
+            hipEventRecord(gpuEventStop);
+            hipDeviceSynchronize();
+            hipEventElapsedTime(&timetaken, gpuEventStart, gpuEventStop);
+            mpitransfertimes.push_back(timetaken*_GPU_TO_SECONDS);
+        }
+        hipEventDestroy(gpuEventStart);
+        hipEventDestroy(gpuEventStop);
+        hipFree(gpu_p1[idev]);
+        hipFree(gpu_p2[idev]);
+    }
+    // Calculate avg time and effective bandwidth
+    double avg_time = 0.0, total_time = 0.0;
+    for (auto &t:mpitransfertimes)
+    {
+        total_time += t;
+    }
+    avg_time = total_time / mpitransfertimes.size();
+    double bw = opt.msg_size / avg_time;
+    // Report time, bw, and message size
+    Rank0LocalLoggerWithTime()
+        << "Test " << mpifunc << " finished with avg communication time of " << avg_time << " seconds"
+        << ", with effective bandwidth " << bw << " GB/s " 
+        << "and message size of " << opt.msg_size << " GB." << std::endl;
+    mpitransfertimes.clear();
+}
+
+/// @brief Test whether simple send/receive works and check bandwidth
+/// @param opt Options struct containing runtime information
+void MPITestGPUBandwidthSendRecv(Options &opt){
+    if (NProcs<2) return;
+    MPI_Status status;
+    std::string mpifunc;
+    std::vector<double> senddata, receivedata;
+    
+    double * p1 = nullptr, *p2 = nullptr;
+    std::vector<double*> gpu_p1, gpu_p2;
+    std::vector<float> mpitransfertimes;
+    double nbytes = opt.msg_size * 1024.0 * 1024.0 * 1024.0;
+    double nelements = nbytes / sizeof(double);
+
+    int nDevices;
+    hipGetDeviceCount(&nDevices);
+    gpu_p1.resize(nDevices);
+    gpu_p2.resize(nDevices);
+    for (auto &x:gpu_p1) x=nullptr;
+    for (auto &x:gpu_p2) x=nullptr;
+
+    // now bandwidth measurement for send receive 
+    mpifunc = "GPU_bandwidth_sendrecv";
+    LogMPITest();
+    senddata.resize(nelements);
+    receivedata.resize(nelements);
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipMalloc((void**)&gpu_p1[idev], nbytes);
+        hipMalloc((void**)&gpu_p2[idev], nbytes);
+        hipMemcpy(gpu_p1[idev], senddata.data(), nbytes, hipMemcpyHostToDevice);
+    }
+    for (auto &d:senddata) d = pow(2.0,ThisTask);
+    float timetaken;
+    for (auto idev=0; idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        timetaken=0.0;
+        hipEvent_t gpuEventStart, gpuEventStop;
+        hipEventCreate(&gpuEventStart);
+        hipEventCreate(&gpuEventStop);
+        p1 = gpu_p1[idev];
+        p2 = gpu_p2[idev];
+        std::vector<float> times;
+        for (auto itask=0;itask<NProcs;itask++) {
+            if (itask == 0) continue;
+            if (!(ThisTask==0 or ThisTask==itask)) continue;
+            int tag = 100;
+            for (auto iter=0;iter<opt.Niter;iter++) {
+                hipEventRecord(gpuEventStart);
+                if (ThisTask==0) {
+                    MPI_Send(p1, nelements, MPI_DOUBLE, itask, tag, MPI_COMM_WORLD);
+                }
+                else if (ThisTask==itask) {
+                    MPI_Recv(p2, nelements, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD, &status);
+                }
+                hipEventRecord(gpuEventStop);
+                hipDeviceSynchronize();
+                hipEventElapsedTime(&timetaken, gpuEventStart, gpuEventStop);
+                mpitransfertimes.push_back(timetaken*_GPU_TO_SECONDS);
+            }
+        }
+    }
+    double avg_time = 0.0, total_time = 0.0;
+    for (auto &t:mpitransfertimes)
+    {
+        total_time += t;
+    }
+    avg_time = total_time / mpitransfertimes.size();
+    double bw = opt.msg_size / avg_time;
+    Rank0LocalLoggerWithTime()
+        << "Test " << mpifunc << " finished with avg communication time of " << avg_time << " seconds"
+        << ", with effective bandwidth " << bw << " GB/s " 
+        << "and message size of " << opt.msg_size << " GB." << std::endl;
+    mpitransfertimes.clear();
+
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipFree(gpu_p1[idev]);
+        hipFree(gpu_p2[idev]);
+    }
+    senddata.clear();
+    senddata.shrink_to_fit();
+    receivedata.clear();
+    receivedata.shrink_to_fit();
+    gpu_p1.clear();
+    gpu_p2.clear();
+    MPI_Barrier(MPI_COMM_WORLD);
+};
+
+/// @brief Test whether GPU-GPU Asynchronous communication works
+/// @param opt Options struct containing runtime information
+void MPITestGPUAsyncSendRecv(Options &opt){
+    if (NProcs<2) return;
+    std::string mpifunc;
+    std::vector<double> senddata, receivedata;
+    
+    double * p1 = nullptr, *p2 = nullptr;
+    std::vector<double*> gpu_p1, gpu_p2;
+    std::vector<float> mpitransfertimes;
+    double nbytes = opt.msg_size * 1024.0 * 1024.0 * 1024.0;
+    double nelements = nbytes / sizeof(double);
+
+    int nDevices;
+    hipGetDeviceCount(&nDevices);
+    gpu_p1.resize(nDevices);
+    gpu_p2.resize(nDevices);
+    for (auto &x:gpu_p1) x=nullptr;
+    for (auto &x:gpu_p2) x=nullptr;
+
+    // now allreduce 
+    mpifunc = "GPU_async_sendrecv";
+    LogMPITest();
+    senddata.resize(nelements);
+    receivedata.resize(nelements);
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipHostMalloc((void**)&gpu_p1[idev], nbytes);
+        hipHostMalloc((void**)&gpu_p2[idev], nbytes);
+        hipMemcpy(gpu_p1[idev], senddata.data(), nbytes, hipMemcpyHostToDevice);
+    }
+    for (auto &d:senddata) d = pow(2.0,ThisTask);
+    float timetaken;
+    // need to move this to an async mpi test 
+    for (auto idev=0; idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        p1 = gpu_p1[idev];
+        p2 = gpu_p2[idev];
+        timetaken=0.0;
+        hipEvent_t gpuEventStart, gpuEventStop;
+        hipEventCreate(&gpuEventStart);
+        hipEventCreate(&gpuEventStop);
+        for (auto iter=0;iter<opt.Niter;iter++) {
+            hipEventRecord(gpuEventStart);
+            std::vector<MPI_Request> sendreqs, recvreqs;
+            for (auto isend=0;isend<NProcs;isend++) {
+                if (isend != ThisTask) 
+                {
+                    MPI_Request request;
+                    int tag = isend * NProcs + ThisTask + idev;
+                    MPI_Isend(p1, nelements, MPI_DOUBLE, isend, tag, MPI_COMM_WORLD, &request);
+                    sendreqs.push_back(request);
+                }
+            }
+            for (auto irecv=0;irecv<NProcs;irecv++) {
+                if (irecv != ThisTask) 
+                {
+                    MPI_Request request;
+                    int tag = ThisTask * NProcs + irecv+idev;
+                    MPI_Irecv(p2, nelements, MPI_DOUBLE, irecv, tag, MPI_COMM_WORLD, &request);
+                    recvreqs.push_back(request);
+                }
+            }
+            MPI_Waitall(sendreqs.size(), sendreqs.data(), MPI_STATUSES_IGNORE);
+            MPI_Waitall(recvreqs.size(), recvreqs.data(), MPI_STATUSES_IGNORE);
+            hipEventRecord(gpuEventStop);
+            hipDeviceSynchronize();
+            hipEventElapsedTime(&timetaken, gpuEventStart, gpuEventStop);
+            mpitransfertimes.push_back(timetaken*_GPU_TO_SECONDS);
+        }
+        MPI_Barrier(MPI_COMM_WORLD);
+        hipEventDestroy(gpuEventStart);
+        hipEventDestroy(gpuEventStop);
+    }
+    double avg_time = 0.0, total_time = 0.0;
+    for (auto &t:mpitransfertimes)
+    {
+        total_time += t;
+    }
+    avg_time = total_time / mpitransfertimes.size();
+    double total_bytes = 2.0 * (NProcs - 1) * opt.msg_size;
+    double bw = total_bytes / avg_time;
+    Rank0LocalLoggerWithTime()
+        << "Test " << mpifunc << " finished with avg communication time of " << avg_time << " seconds"
+        << ", with effective bandwidth " << bw << " GB/s " 
+        << "and message size of " << opt.msg_size << " GB." << std::endl;
+    mpitransfertimes.clear();
+
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipFree(gpu_p1[idev]);
+        hipFree(gpu_p2[idev]);
+    }
+    senddata.clear();
+    senddata.shrink_to_fit();
+    receivedata.clear();
+    receivedata.shrink_to_fit();
+    gpu_p1.clear();
+    gpu_p2.clear();
+    MPI_Barrier(MPI_COMM_WORLD);
+};
+
+/// @brief Test collective GPU-GPU communication works
+/// @param opt Options struct containing runtime information
+void MPITestGPUAllGather(Options &opt){
+    if (NProcs<2) return;
+    std::string mpifunc;
+    std::vector<double> senddata, receivedata;
+    
+    double * p1 = nullptr, *p2 = nullptr;
+    std::vector<double*> gpu_p1, gpu_p2;
+    std::vector<float> mpitransfertimes;
+    double nbytes = opt.msg_size * 1024.0 * 1024.0 * 1024.0;
+    double nelements = nbytes / sizeof(double);
+
+    int nDevices;
+    hipGetDeviceCount(&nDevices);
+    gpu_p1.resize(nDevices);
+    gpu_p2.resize(nDevices);
+    for (auto &x:gpu_p1) x=nullptr;
+    for (auto &x:gpu_p2) x=nullptr;
+
+    mpifunc = "GPU_allgather";
+    LogMPITest();
+    senddata.resize(nelements);
+    receivedata.resize(nelements * NProcs);
+    for (auto &d:senddata) d = pow(2.0,ThisTask);
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipMalloc((void**)&gpu_p1[idev], nbytes);
+        hipMalloc((void**)&gpu_p2[idev], nbytes * NProcs);
+        hipMemcpy(gpu_p1[idev], senddata.data(), nbytes, hipMemcpyHostToDevice);
+    }
+    float timetaken;
+    for (auto idev=0; idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        timetaken=0.0;
+        hipEvent_t gpuEventStart, gpuEventStop;
+        hipEventCreate(&gpuEventStart);
+        hipEventCreate(&gpuEventStop);
+        p1 = gpu_p1[idev];
+        p2 = gpu_p2[idev];
+        for (auto iter=0;iter<opt.Niter;iter++) {
+            hipEventRecord(gpuEventStart);
+            MPI_Allgather(p1, nelements, MPI_DOUBLE, p2, nelements, MPI_DOUBLE, MPI_COMM_WORLD);
+            hipEventRecord(gpuEventStop);
+            hipDeviceSynchronize();
+            hipEventElapsedTime(&timetaken, gpuEventStart, gpuEventStop);
+            mpitransfertimes.push_back(timetaken*_GPU_TO_SECONDS);
+        }
+    }
+    double avg_time = 0.0, total_time = 0.0;
+    for (auto &t:mpitransfertimes)
+    {
+        total_time += t;
+    }
+    avg_time = total_time / mpitransfertimes.size();
+    double bw = opt.msg_size / avg_time;
+    Rank0LocalLoggerWithTime()
+        << "Test " << mpifunc << " finished with avg communication time of " << avg_time << " seconds"
+        << ", with effective bandwidth " << bw << " GB/s " 
+        << "and message size of " << opt.msg_size << " GB." << std::endl;
+    mpitransfertimes.clear();
+
+    for (auto idev=0;idev<nDevices;idev++) {
+        hipSetDevice(idev);
+        hipFree(gpu_p1[idev]);
+        hipFree(gpu_p2[idev]);
+    }
+    senddata.clear();
+    senddata.shrink_to_fit();
+    receivedata.clear();
+    receivedata.shrink_to_fit();
+    gpu_p1.clear();
+    gpu_p2.clear();
+    MPI_Barrier(MPI_COMM_WORLD);
+};
+
+//@}
+
+void MPIRunTests(Options &opt)
+{
+    MPITestGPUCopy(opt);
+    MPITestGPUBandwidthSendRecv(opt);
+    MPITestGPUAsyncSendRecv(opt);
+    MPITestGPUAllGather(opt);
+}
+
+int main(int argc, char **argv) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Comm_size(comm, &NProcs);
+    MPI_Comm_rank(comm, &ThisTask);
+    Options opt;
+    GetArgs(argc, argv, opt);
+
+    Rank0LocalLogger()<<"Starting job "<<std::endl;
+    MPI_Barrier(comm);
+
+    // Run the tests
+    MPIRunTests(opt);
+
+    if (ThisTask == 0) {
+        char version[MPI_MAX_LIBRARY_VERSION_STRING];
+        int len = 0;
+        MPI_Get_library_version(version, &len);
+        printf("\n=== MPI runtime ===\n%.*s\n", len, version);
+        printf("\nTEST_07_GPU-MPI_CONTAINER_SUCCESS size=%d\n", NProcs);
+    }
+    MPI_Finalize();
+    return 0;
+}
---
+++ setonix/mpi/tests/test_07_basic-gpu-mpi-comm.slurm.sh
@@ -0,0 +1,292 @@
+#!/bin/bash --login
+#SBATCH --job-name=test_07_gpu-mpi-comm
+#SBATCH --nodes=1
+#SBATCH -n 2
+#SBATCH --ntasks-per-node=2
+#SBATCH --gres=gpu:2
+#SBATCH --time=00:15:00
+##SBATCH --partition=gpu
+#SBATCH --partition=gpu-dev
+#SBATCH --output=slurm-%x-%j.out
+#SBATCH --error=slurm-%x-%j.err
+#SBATCH --account=pawsey0001-gpu
+
+# Focus:
+# This test validates execution of the test:
+# - test_gpu_mpi_comm: GPU-MPI communication tests.
+# a trimmed down version of what is found in the profile_util library
+
+# Note:
+# This script is based on a script that has been developed by Alexis Espinosa with the help of Microsoft 360 Copilot - GPT 5.5.
+
+# Normally this script is submitted by one of the product-specific launchers like:
+#
+#   pawsey-containers/setonix/mpi/mpich-base/testing/run_tests.sh
+#   pawsey-containers/setonix/mpi/lustrempich-base/testing/run_tests.sh
+#   pawsey-containers/setonix/mpi/rocm-mpich-base/testing/run_tests.sh
+#
+# Those launchers define the required environment variables and submit this
+# script with sbatch.
+#
+# Manual submission example, from the relevant product testing directory:
+#
+#   export REPO_MPI_DIR="/path/to/repo/pawsey-containers/setonix/mpi"
+#   export SINGULARITY_IMAGE="/path/to/image.sif"
+#   export SINGULARITY_MODULE="singularity/4.1.0-mpi"
+#   sbatch --export=REPO_MPI_DIR,SINGULARITY_IMAGE,SINGULARITY_MODULE \
+#       /path/to/repo/pawsey-containers/setonix/mpi/tests/test_07_gpu-mpi-comm.slurm.sh
+#
+# REPO_MPI_DIR must point to the repository's pawsey-containers/setonix/mpi directory.
+# SINGULARITY_IMAGE must point to the container image being tested.
+# SINGULARITY_MODULE defaults to singularity/4.1.0-mpi when not exported.
+
+#--- Strict mode
+set -euo pipefail
+
+#--- Name used for output files and markers
+: "${SLURM_JOB_NAME:?SLURM_JOB_NAME is not set}"
+TEST_NAME="${SLURM_JOB_NAME}"
+
+#--- Important variables to be provided as environment variable
+# The singularity image to use:
+: "${SINGULARITY_IMAGE:?SINGULARITY_IMAGE is not set}"
+# The Singularity module to load (default supports direct submission):
+SINGULARITY_MODULE="${SINGULARITY_MODULE:-singularity/4.1.0-mpi}"
+# The path of the mpi subdirectory in the repository (needed as reference to find the rest of the stuff):
+: "${REPO_MPI_DIR:?REPO_MPI_DIR is not set. Submit this test through a run_tests.sh script or export REPO_MPI_DIR manually.}"
+
+#--- Basic path setup
+LAUNCH_DIR="${SLURM_SUBMIT_DIR:-$PWD}"
+SHARED_TESTS_DIR="$REPO_MPI_DIR/tests"
+FIXTURES_DIR="$SHARED_TESTS_DIR/fixtures"
+TESTS_SUPPORT_DIR="$SHARED_TESTS_DIR/tests-support"
+
+ARTIFACTS_ROOT_DIR="${LAUNCH_DIR}/artifacts"
+RUN_ID="${CI_PIPELINE_ID:-$(date -u +%Y%m%dT%H%M%SZ)-$$}"
+ARTIFACTS_DIR="${ARTIFACTS_DIR:-${ARTIFACTS_ROOT_DIR}/singleruns/${TEST_NAME}/${RUN_ID}}"
+
+BUILD_DIR="${BUILD_DIR:-${ARTIFACTS_DIR}/build}"
+OUTPUT_DIR="${OUTPUT_DIR:-${ARTIFACTS_DIR}/output}"
+PASS_MARKER="$OUTPUT_DIR/${TEST_NAME}.PASS"
+FAIL_MARKER="$OUTPUT_DIR/${TEST_NAME}.FAIL"
+WARN_MARKER="$OUTPUT_DIR/${TEST_NAME}.WARN"
+WARN_COUNT=0
+
+mkdir -p "$BUILD_DIR" "$OUTPUT_DIR"
+
+rm -f "$PASS_MARKER" "$FAIL_MARKER" "$WARN_MARKER"
+
+#--- Helper functions
+fail() {
+    local reason="$*"
+    echo
+    echo "${TEST_NAME}: FAIL"
+    echo "Reason: ${reason}" >&2
+    {
+        echo "${TEST_NAME}: FAIL"
+        echo "Reason: ${reason}"
+    } > "${FAIL_MARKER}"
+    exit 1
+}
+
+warn() {
+    local msg="$*"
+    echo
+    echo "WARNING: ${msg}" >&2
+    WARN_COUNT=$((WARN_COUNT + 1))
+    {
+        echo "WARNING: ${msg}"
+    } >> "${WARN_MARKER}"
+}
+
+pass() {
+    echo
+    if [[ "${WARN_COUNT}" -gt 0 ]]; then
+        echo "${TEST_NAME}: PASS with ${WARN_COUNT} warning(s)"
+        {
+            echo "PASS_WITH_WARNINGS"
+            echo "Warnings: ${WARN_COUNT}"
+            echo "Warning marker: ${WARN_MARKER}"
+        } > "${PASS_MARKER}"
+    else
+        echo "${TEST_NAME}: PASS"
+        {
+            echo "PASS"
+        } > "${PASS_MARKER}"
+    fi
+    exit 0
+}
+
+compare_float_gt() {
+    local lhs="$1"
+    local rhs="$2"
+
+    awk -v lhs="${lhs}" -v rhs="${rhs}" 'BEGIN { exit !(lhs > rhs) }'
+}
+
+extract_max_tag_world_average_usec() {
+    local test_name="$1"
+    local input_file="$2"
+
+    awk -v test_name="${test_name}" '
+        /MPI Comm=Tag_world/ && $0 ~ test_name && /timing \[ave,std,min,max\]/ {
+            if (match($0, /timing \[ave,std,min,max\]=\[[^]]+\]/)) {
+                metric = substr($0, RSTART, RLENGTH)
+                gsub(/^timing \[ave,std,min,max\]=\[/, "", metric)
+                gsub(/\]$/, "", metric)
+                split(metric, values, ",")
+                ave = values[1] + 0
+                if (ave > max_ave) {
+                    max_ave = ave
+                }
+                count++
+            }
+        }
+        END {
+            if (count > 0) {
+                print max_ave
+            }
+        }
+    ' "${input_file}"
+}
+
+#--- Resolve absolute path for REPO_MPI_DIR
+if ! REPO_MPI_DIR="$(cd "${REPO_MPI_DIR}" && pwd)"; then
+    fail "REPO_MPI_DIR does not exist or is not accessible: ${REPO_MPI_DIR}"
+fi
+
+#--- Image selected by launcher
+if [[ ! -f "${SINGULARITY_IMAGE}" ]]; then
+    fail "Singularity image not found: ${SINGULARITY_IMAGE}"
+fi
+echo "Using image: $SINGULARITY_IMAGE"
+echo "Using Singularity module: $SINGULARITY_MODULE"
+echo "Launch directory: $LAUNCH_DIR"
+echo "MPI directory: $REPO_MPI_DIR"
+echo "Shared tests directory: $SHARED_TESTS_DIR"
+echo "Fixtures directory: $FIXTURES_DIR"
+echo "Build directory: $BUILD_DIR"
+echo "Output directory: $OUTPUT_DIR"
+
+#--- Modules and settings
+module load "${SINGULARITY_MODULE}"
+
+if [[ "${PAWSEY_CLUSTER:-}" == "joey" ]]; then
+    source "${TESTS_SUPPORT_DIR}/common.Joey.settings.sh"
+fi
+
+module list
+
+#--- MPI and Slingshot settings
+if [[ "${SLURM_JOB_NUM_NODES:-}" -gt 1 ]]; then
+    echo "Running on multiple nodes: ${SLURM_JOB_NUM_NODES}"
+    echo "Setting MPICH_OFI_STARTUP_CONNECT=1 and MPICH_OFI_VERBOSE=1 for multi-node runs"
+    export MPICH_OFI_STARTUP_CONNECT=1
+    export MPICH_OFI_VERBOSE=1
+else
+    echo "Running on a single node: ${SLURM_JOB_NUM_NODES:-1}"
+fi
+
+#Setting a random VNI for the test to avoid conflicts with other jobs on the same node
+export FI_CXI_DEFAULT_VNI
+FI_CXI_DEFAULT_VNI="$(od -vAn -N4 -tu < /dev/urandom)"
+
+#echo "MPICH_OFI_STARTUP_CONNECT=${MPICH_OFI_STARTUP_CONNECT}"
+#echo "MPICH_OFI_VERBOSE=${MPICH_OFI_VERBOSE}"
+echo "FI_CXI_DEFAULT_VNI=${FI_CXI_DEFAULT_VNI}"
+
+#--- Input/output files
+src="${FIXTURES_DIR}/gpu-mpi_comms.cpp"
+theExe="${BUILD_DIR}/gpu-mpi_comms.exe"
+
+fileOutCompile="${OUTPUT_DIR}/res_${TEST_NAME}.compile.out"
+fileOutLinkage="${OUTPUT_DIR}/res_${TEST_NAME}.linkage.out"
+fileOutRun="${OUTPUT_DIR}/res_${TEST_NAME}.run.out"
+
+if [[ ! -f "${src}" ]]; then
+    fail "Fixture source file not found: ${src}"
+fi
+
+# Extra libraries required for GPU-aware MPI linked against GTL library
+export SINGULARITY_BINDPATH=/opt/cray/pe/lib64/libmpi_gnu_123.so.12,/opt/cray/pe/mpich/8.1.32/ofi/gnu/12.3/lib/libmpi_gtl_hsa.so,$SINGULARITY_BINDPATH
+export SINGULARITYENV_LD_PRELOAD=/opt/cray/pe/lib64/libmpi_gnu_123.so.12:/opt/cray/pe/mpich/8.1.32/ofi/gnu/12.3/lib/libmpi_gtl_hsa.so:$SINGULARITYENV_LD_PRELOAD
+
+#--- Compile test
+echo
+echo "=== Building with container hipcc ==="
+if ! singularity exec -B /opt/cray/pe "${SINGULARITY_IMAGE}" \
+    hipcc -D_HIP -D_MPI -std=c++17 -Wno-unused-result \
+    -I${CRAY_MPICH_DIR}/include/ -L${CRAY_MPICH_DIR}/lib/ -L${CRAY_MPICH_ROOTDIR}/gtl/lib/ \
+    "${src}" -o "${theExe}" -lmpi -lmpi_gtl_hsa \
+    > "${fileOutCompile}" 2>&1; then
+    fail "Compilation failed. See: ${fileOutCompile}"
+fi
+
+if [[ ! -x "${theExe}" ]]; then
+    fail "Expected executable was not created or is not executable: ${theExe}"
+fi
+
+echo "Compilation succeeded: ${theExe}"
+
+#--- Linkage test
+echo
+echo "=== Linkage check ==="
+
+if ! singularity exec -B /opt/cray/pe "${SINGULARITY_IMAGE}" bash -lc \
+    "ldd \"\$(command -v osu_latency)\" | grep -E 'mpi|fabric|cxi|pmi|pmix|pals|xpmem|gtl|hsa' || true" \
+    | tee "${fileOutLinkage}"; then
+    fail "Linkage command failed. See: ${fileOutLinkage}"
+fi
+
+if ! grep -Fq "/opt/cray/pe/mpich" "${fileOutLinkage}"; then
+    fail "Expected osu_latency to link against /opt/cray/pe/mpich. See: ${fileOutLinkage}"
+fi
+
+if ! grep -Fq "/opt/cray/libfabric" "${fileOutLinkage}"; then
+    fail "Expected osu_latency to link against /opt/cray/libfabric. See: ${fileOutLinkage}"
+fi
+
+if ! grep -Fq "gtl_hsa.so" "${fileOutLinkage}"; then
+    fail "Expected osu_latency to link against GTL library. See: ${fileOutLinkage}"
+fi
+
+echo "Linkage check passed"
+
+#--- Runtime test
+echo
+echo "=== Running on ${SLURM_JOB_NUM_NODES} nodes ==="
+
+TOTAL_TASKS=$((SLURM_JOB_NUM_NODES * SLURM_TASKS_PER_NODE))
+
+if ! srun -N "${SLURM_JOB_NUM_NODES}" \
+     --ntasks-per-node=${SLURM_TASKS_PER_NODE} --gres=gpu:${SLURM_NTASKS_PER_NODE} \
+     singularity exec -B /opt/cray/pe "${SINGULARITY_IMAGE}" "${theExe}" \
+     | tee "${fileOutRun}"; then
+    fail "Runtime execution failed. See: ${fileOutRun}"
+fi
+
+expected_success_line="TEST_07_GPU-MPI_CONTAINER_SUCCESS size=${TOTAL_TASKS}"
+
+if ! grep -Fq "${expected_success_line}" "${fileOutRun}"; then
+    fail "Expected success line not found: ${expected_success_line}. See: ${fileOutRun}"
+fi
+
+if ! grep -Fq "running GPU_GPU_copy test" "${fileOutRun}"; then
+    fail "test_gpu-mpi_comm output missing gpu-gpu copy test. See: ${fileOutRun}"
+fi
+
+if ! grep -Fq "running GPU_bandwidth_sendrecv test" "${fileOutRun}"; then
+    fail "test_gpu-mpi_comm output missing sendrecv test. See: ${fileOutRun}"
+fi
+
+if ! grep -Fq "running GPU_async_sendrecv test" "${fileOutRun}"; then
+    fail "test_gpu-mpi_comm output missing async sendrecv test. See: ${fileOutRun}"
+fi
+
+if ! grep -Fq "running GPU_allgather test" "${fileOutRun}"; then
+    fail "test_gpu-mpi_comm output missing allreduce test. See: ${fileOutRun}"
+fi
+
+echo "Runtime check passed"
+
+pass
